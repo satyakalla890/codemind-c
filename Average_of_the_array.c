0acdfd2743c91668a7335c4179c1c10dbfbcd7de
satyakalla890 <147488170+satyakalla890@users.noreply.github.com>
2023-12-18 15:09:19 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n],sum=0;     for (i=0;i<n;i++)     {         scanf("%d",&arr[i]);             sum=sum+arr[i];     }     float avr=(float)sum/n;     printf("%.2f",avr); }

---
+++ Average_of_the_array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n],sum=0;
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+            sum=sum+arr[i];
+    }
+    float avr=(float)sum/n;
+    printf("%.2f",avr);
+}
